e4760db100716a65086e9726a4e598f44a2802ea
quoc <lyhangquoc274@gmail.com>
2024-02-19 22:53:56 +0700

    update

--- src/main/java/com/example/springdemo/repository/UserRepo.java
+++ src/main/java/com/example/springdemo/repository/UserRepo.java
@@ -4,6 +4,7 @@ import com.example.springdemo.model.State;
 import com.example.springdemo.model.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,6 +12,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class UserRepo {
     private ConcurrentHashMap<String , User> users = new ConcurrentHashMap<>();
 
+    public User addUser(String fullname , String email, String hashed_password){
+        return addUser(fullname,email,hashed_password,State.PENDING);
+    }
     public User addUser(String fullname , String email, String hashed_password, State state){
         String id = UUID.randomUUID().toString();
         User user = User.builder()
@@ -18,8 +22,17 @@ public class UserRepo {
                 .fullname(fullname)
                 .email(email)
                 .hashed_password(hashed_password)
+                .state(state)
                 .build();
         users.put(id,user);
         return user;
+    }
+
+    public boolean isEmailExist(String email){
+        return users.values().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).count() > 0;
+    }
+
+    public Optional<User> findByEmail(String email){
+        return users.values().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
     }
 }
--- src/test/java/com/example/springdemo/TestUserRepo.java
+++ src/test/java/com/example/springdemo/TestUserRepo.java
@@ -1,4 +1,54 @@
 package com.example.springdemo;
 
+import com.example.springdemo.model.State;
+import com.example.springdemo.model.User;
+import com.example.springdemo.repository.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
 public class TestUserRepo {
+    @Test
+    public void addUser(){
+        UserRepo userRepo = new UserRepo();
+        User user = userRepo.addUser("John Levy" , "levy@gmail.com" , "0X-1321am21321" , State.PENDING);
+        assertThat(user).isNotNull();
+        System.out.println(user.getId());
+        assertThat(user.getId()).isNotBlank();
+    }
+
+    @Test
+    public void addUserWithPendingState(){
+        UserRepo userRepo = new UserRepo();
+        User user = userRepo.addUser("John Levy" , "levy@gmail.com" , "0X-1321am21321" );
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isNotBlank();
+        assertThat(user.getState()).isEqualTo(State.PENDING);
+    }
+
+    @Test
+    public void isEmailExist(){
+        UserRepo userRepo = new UserRepo();
+        userRepo.addUser("John Levy" , "levy@gmail.com" , "0X-1321am21321" );
+        userRepo.addUser("Quoc" , "quoc@gmail.com" , "0X-13221321" );
+        userRepo.addUser("khanh" , "khanh@gmail.com" , "0X-1321am21331231221" );
+        assertThat(userRepo.isEmailExist("levy@gmail.com")).isTrue();
+        assertThat(userRepo.isEmailExist("quoC@gmail.com")).isTrue();
+        assertThat(userRepo.isEmailExist("khanh@gmail.com")).isTrue();
+        assertThat(userRepo.isEmailExist("Khanh@gmail.com")).isTrue();
+        assertThat(userRepo.isEmailExist("khanh12@gmail.com")).isFalse();
+    }
+
+    @Test
+    public void findByEmail(){
+        UserRepo userRepo = new UserRepo();
+        userRepo.addUser("John Levy" , "levy@gmail.com" , "0X-1321am21321" );
+        userRepo.addUser("Quoc" , "quoc@gmail.com" , "0X-13221321" );
+        userRepo.addUser("khanh" , "khanh@gmail.com" , "0X-1321am21331231221" );
+        assertThat(userRepo.findByEmail("levy@gmail.com")).isPresent();
+        assertThat(userRepo.findByEmail("quoC@gmail.com")).isPresent();
+        assertThat(userRepo.findByEmail("Khanh@gmail.com")).isPresent();
+        assertThat(userRepo.findByEmail("khanh12@gmail.com")).isNotPresent();
+    }
 }
